9430005b107c1a6a0377a1babab738103d1c4765
Suresh Kumar S <suricekmar12@gmail.com>
2023-03-06 16:35:18 +0530

    Add files via upload
    call back hell code

---
+++ script.js
@@ -0,0 +1,53 @@
+var counter = document.getElementById("count");
+let time = 10;
+setTimeout(() => {
+    time--
+    counter.innerText = time;
+
+    setTimeout(() => {
+        time--
+        counter.innerText = time;
+
+        setTimeout(() => {
+            time--
+            counter.innerText = time;
+
+            setTimeout(() => {
+                time--
+                counter.innerText = time;
+
+                setTimeout(() => {
+                    time--
+                    counter.innerText = time;
+
+                    setTimeout(() => {
+                        time--
+                        counter.innerText = time;
+
+                        setTimeout(() => {
+                            time--
+                            counter.innerText = time;
+
+                            setTimeout(() => {
+                                time--
+                                counter.innerText = time;
+
+                                setTimeout(() => {
+                                    time--
+                                    counter.innerText = time;
+
+
+                                    setTimeout(() => {
+                                        time--
+                                        counter.innerText = "Happy Independence Day🥳🎊🎉🎉🎉";
+
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
